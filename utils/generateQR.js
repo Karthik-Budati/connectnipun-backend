6f4c6e38f6abc74ad5f4255cff41908a98863e64
Karthik-Budati <karthikbudati.666@gmail.com>
2025-07-25 19:21:30 +0530

    Update generateQR.js

--- utils/generateQR.js
+++ utils/generateQR.js
@@ -11,7 +11,7 @@ cloudinary.config({
 });
 
 async function generateQR(worker) {
-  const profileURL = `https://connectnipun.onrender.com/worker/${worker._id}`;
+  const profileURL = `https://connectnipun.onrender.com/api/workers/public/${worker_id}`;
 
   try {
     const canvas = createCanvas(300, 300);
